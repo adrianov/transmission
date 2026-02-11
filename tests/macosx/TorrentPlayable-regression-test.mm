d57725cefe4748bc55c1219bb0f1290f73ba470c
Peter M Adrianov <peter.adrianov@gmail.com>
2026-02-11 16:01:34 +0300

    test: add regression test for document-books title preservation

--- tests/macosx/TorrentPlayable-regression-test.mm
+++ tests/macosx/TorrentPlayable-regression-test.mm
@@ -12,10 +12,7 @@
 TEST(TorrentPlayableRegressionTest, PlayableEntryNameUpdateRequiresMutableDict)
 {
     NSMutableArray<NSDictionary*>* playable = [NSMutableArray array];
-    [playable addObject:[@{
-        @"name" : @"Show - Episode One (2024)",
-        @"relativePath" : @"Show/Episode.One.mkv"
-    } mutableCopy]];
+    [playable addObject:[@{ @"name" : @"Show - Episode One (2024)", @"relativePath" : @"Show/Episode.One.mkv" } mutableCopy]];
 
     NSString* commonPrefix = @"Show - ";
     NSString* commonSuffix = @" (2024)";
@@ -38,4 +35,45 @@ TEST(TorrentPlayableRegressionTest, PlayableEntryNameUpdateRequiresMutableDict)
 
     EXPECT_TRUE([playable[0][@"name"] isEqualToString:@"Episode One"])
         << "Stripped name should be 'Episode One', got: " << [playable[0][@"name"] UTF8String];
+}
+
+TEST(TorrentPlayableRegressionTest, DocumentBooksPreserveFullTitleWhenMixedWithAudio)
+{
+    // PDF mixed with audio: per-parent strip must skip document-books so "In The Court Of The Crimson King..."
+    // and "Larks' Tongues In Aspic..." stay full, not truncated to "In" or "Larks'".
+    NSMutableArray<NSDictionary*>* playable = [NSMutableArray array];
+    [playable addObject:[@{
+                  @"type" : @"document-books",
+                  @"name" : @"In The Court Of The Crimson King (Expanded & Remastered Original Album Mix)",
+                  @"relativePath" : @"Album/In The Court Of The Crimson King (Expanded & Remastered Original Album Mix).pdf"
+              } mutableCopy]];
+    [playable addObject:[@{
+                  @"type" : @"file",
+                  @"name" : @"05-The Court Of The Crimson King",
+                  @"relativePath" : @"Album/05-The Court Of The Crimson King.flac"
+              } mutableCopy]];
+
+    NSString* commonPrefix = @"";
+    NSString* commonSuffix = @"";
+    for (NSMutableDictionary* fileInfo in playable)
+    {
+        if ([fileInfo[@"type"] isEqualToString:@"document-books"])
+            continue;
+        NSString* relativePath = fileInfo[@"relativePath"];
+        if (relativePath.length == 0)
+            continue;
+        NSString* title = fileInfo[@"name"];
+        if (title.length == 0)
+            continue;
+        if (commonPrefix.length > 0 && title.length > commonPrefix.length && [title hasPrefix:commonPrefix])
+            title = [title substringFromIndex:commonPrefix.length];
+        if (commonSuffix.length > 0 && title.length > commonSuffix.length && [title hasSuffix:commonSuffix])
+            title = [title substringToIndex:title.length - commonSuffix.length];
+        title = [title stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceAndNewlineCharacterSet];
+        if (title.length > 0)
+            fileInfo[@"name"] = title;
+    }
+
+    EXPECT_TRUE([playable[0][@"name"] isEqualToString:@"In The Court Of The Crimson King (Expanded & Remastered Original Album Mix)"])
+        << "PDF title must stay full, got: " << [playable[0][@"name"] UTF8String];
 }
